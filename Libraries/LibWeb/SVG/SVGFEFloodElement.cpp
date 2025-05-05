6065a6a83992d68d3cf2e1f5328cb6c73522e655
Lucien Fiorini <lucienfiorini@gmail.com>
2025-05-05 10:40:05 +0200

    LibWeb/SVG: Add SVGFEFloodElement

---
+++ Libraries/LibWeb/SVG/SVGFEFloodElement.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025, Lucien Fiorini <lucienfiorini@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/SVGFEFloodElementPrototype.h>
+#include <LibWeb/CSS/Parser/Parser.h>
+#include <LibWeb/Layout/Node.h>
+#include <LibWeb/SVG/SVGFEFloodElement.h>
+
+namespace Web::SVG {
+
+GC_DEFINE_ALLOCATOR(SVGFEFloodElement);
+
+SVGFEFloodElement::SVGFEFloodElement(DOM::Document& document, DOM::QualifiedName qualified_name)
+    : SVGElement(document, qualified_name)
+{
+}
+
+void SVGFEFloodElement::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    WEB_SET_PROTOTYPE_FOR_INTERFACE(SVGFEFloodElement);
+}
+
+Optional<Gfx::Color> SVGFEFloodElement::flood_color() const {
+    // TODO
+    return {};
+}
+
+Optional<float> SVGFEFloodElement::flood_opacity() const {
+    // TODO
+    return {};
+}
+
+}
