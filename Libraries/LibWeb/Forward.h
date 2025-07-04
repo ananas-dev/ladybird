316dd41a286e4d585e603f19d622d09630ea298b
Lucien Fiorini <lucienfiorini@gmail.com>
2025-07-04 21:08:28 +0200

    LibWeb/SVG: Add FEGaussianBlurElement

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -960,6 +960,7 @@ class SVGElement;
 class SVGEllipseElement;
 class SVGFEBlendElement;
 class SVGFEFloodElement;
+class SVGFEGaussianBlurElement;
 class SVGFilterElement;
 class SVGForeignObjectElement;
 class SVGGeometryElement;
